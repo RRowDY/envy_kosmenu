3f7c02b041cb4cc5b36bd9ab662c30968d6ea86a
RRowDY <durellwalzer@gmail.com>
2025-12-27 04:24:00 -0700

    Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'Envy Roleplay'
-description 'KOS Menu - Kill on Sight Management System'
+author 'Joshua'
+description 'KOS Menu'
 version '1.0.0'
 
 shared_script 'config.lua'
